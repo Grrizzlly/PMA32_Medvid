edd0b2dba0cffa465300371263b00ae563bc6485
Inspiron\Богдан <bodias97@yandex.ua>
2016-10-06 16:56:14 +0300

    My class

---
+++ Course_project/Course_project/Unit.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project
+{
+    abstract class Unit
+    {
+        public int Id { get; set; }
+        public Unit() { }
+        public Unit(int a)
+        {
+            a = Id; 
+        }
+    }
+
+    class unemployed:Unit
+    {
+
+        public string profesion { get; set; }
+        public string education { get; set; }
+        public string Previous_post { get; set; }
+        public string Previos_place { get; set; }
+        public string Whyfired { get; set; }
+        public string Contacts { get; set; }
+        public string requirements { get; set; }
+        public unemployed() { }
+        public unemployed(string a,string b,string c,string d, string e, string f, string g, int h) :base( h)
+        { 
+            a = profesion;
+                b = education;
+                c = Previous_post;
+                d = Previos_place;
+                e = Whyfired;
+                f = Contacts;
+                g = requirements;
+
+        }
+        
+    }
+
+    class Jobs:Unit
+    {
+        public string Firm { get; set; }
+        public string Position { get; set; }
+        public string working_conditions { get; set; }
+        public string payment  { get; set; }
+        public string requirements_for_specialist { get; set; }
+        public Jobs() { }
+        public Jobs(string a, string b, string c, string d, string e, int h)
+            : base(h)
+        { 
+            a = Firm;
+                b = Position;
+                c = working_conditions;
+                d = payment;
+                e = requirements_for_specialist;
+                
+        }
+
+    }
+
+    class Information : Unit
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public int Year_of_Birth { get; set; }
+        public Information() {}
+        public Information(string a, string b,int c, int h)
+            : base(h)
+        { 
+            a = FirstName;
+                b = SurName;
+                c = Year_of_Birth;
+               
+                
+        }
+    }
+}
