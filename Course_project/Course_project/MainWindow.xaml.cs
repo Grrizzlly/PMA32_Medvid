96441701eef63c470eb4a0d5fc6a732f50977170
Inspiron\Богдан <bodias97@yandex.ua>
2016-12-03 02:57:50 +0200

    Try to Connect with DB.

--- Course_project/Course_project/MainWindow.xaml.cs
+++ Course_project/Course_project/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Data.Entity;
 namespace Course_project
 {
     /// <summary>
@@ -20,9 +21,45 @@ namespace Course_project
     /// </summary>
     public partial class MainWindow : Window
     {
+        employerContext db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new employerContext();
+            db.unemployed_workers.Load(); // загружаемо данні
+            db.full_employment_jobs.Load();
+            db.underemployment_jobs.Load();
+            db.Info.Load();//continue
+            workersGrid.ItemsSource = db.unemployed_workers.Local.ToBindingList(); // устанавливаем привязку к кэшу
+            // workersGrid.ItemsSource = db.full_employment_jobs.Local.ToBindingList();
+            // workersGrid.ItemsSource = db.underemployment_jobs.Local.ToBindingList();
+            // workersGrid.ItemsSource = db.Info.Local.ToBindingList();
+            this.Closing += MainWindow_Closing;
+        }
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Dispose();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (workersGrid.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < workersGrid.SelectedItems.Count; i++)
+                {
+                    unemployed unemp = workersGrid.SelectedItems[i] as unemployed;
+                    if (unemp != null)
+                    {
+                        db.unemployed_workers.Remove(unemp);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            db.SaveChanges();
         }
     }
 }
---
+++ Course_project/Course_project/employerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace Course_project
+{
+    class employerContext :DbContext
+    {
+        public employerContext(): base("DefaultConnection")
+    {
+ 
+    }
+        public DbSet<unemployed> unemployed_workers { get; set; }
+        public DbSet<Full_employment> full_employment_jobs { get; set; }
+        public DbSet<underemployment> underemployment_jobs { get; set; }
+        public DbSet<Information> Info { get; set; }
+    }
+}
